e06d7c94aa6507c131b581129f2fcf8e24085d99
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-09-18 00:15:36 +0200

    website boiler plate complete

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>CyberPlayground</title>
+    </head>
+
+    <body>
+        <h1>Welcome to CyberPlayground</h1>
+        <p>Let's learn together!</p>
+        
+    </body>
+</html>
